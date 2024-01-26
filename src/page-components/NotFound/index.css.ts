f2d22453725edce1e1687c57bc3d484b90d3f506
buysake <sksksk.iex@gmail.com>
2024-01-27 03:48:18 +0900

    feat: Implement NotFoundPage component

---
+++ src/page-components/NotFound/index.css.ts
@@ -0,0 +1,15 @@
+import { style } from '@vanilla-extract/css';
+
+export const contentStyle = style({
+  height: '100vh',
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+export const titleStyle = style({
+  fontSize: '3rem',
+  letterSpacing: '0.2rem',
+  color: '#fff',
+});
---
+++ src/page-components/NotFound/index.tsx
@@ -0,0 +1,9 @@
+import { contentStyle, titleStyle } from './index.css';
+
+export const NotFoundPage = () => {
+  return (
+    <div className={contentStyle}>
+      <h1 className={titleStyle}>404</h1>
+    </div>
+  );
+};
