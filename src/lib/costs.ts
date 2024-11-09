26e520aaed43b847646265aa1bbcdc4223266970
buysake <sksksk.iex@gmail.com>
2024-11-09 22:43:46 +0900

    fix: consts

--- src/lib/costs.ts
+++ src/lib/costs.ts
@@ -5,7 +5,6 @@ export const AIRDROP_TYPE_COLORS: {
 } = {
   game: '#12dc88',
   tech: '#499bea',
-  easy: '#ef6f08',
   daily: '#ffb545',
   only_gas: '#9886e5',
   free: '#c9c940',
@@ -17,7 +16,6 @@ export const AIRDROP_TYPE_NAMES: {
 } = {
   game: 'ゲーム系',
   tech: '技術タスク有',
-  easy: '手軽',
   daily: 'デイリータスク有',
   only_gas: 'gas代のみ',
   free: '原資不要',
